0dc977916ece3be2e7e12768dbf02a2bbc227d40
rudemoos <m.bloemendaal210@gmail.com>
2023-09-06 13:49:03 +0200

    Changes from activiteiten

--- App/activiteiten.php
+++ App/activiteiten.php
@@ -5,10 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/activiteiten.css">
 </head>
 <body>
-    <div>
-
+    <?php
+    include "header.php";
+    ?>
+    <div class="Product-main">
+    <div class="ProductCard">
+                <a class="card">
+                    <div class="ImageProduct">
+                        <img src="images/teamuitje.jpg" class="ImgCard"/>
+                    </div>
+                    <div class="TxtCard">
+                        Solex rijden
+                    </div>
+                    <div class="TxtCard">
+                            Kom mee solex rijden in het prachtige veluwe
+                    </div>
+                </a>
+            </div>
+            <div class="ProductCard">
+                <a class="card">
+                    <div class="ImageProduct">
+                        <img src="images/Wipeout.jpg" class="ImgCard"/>
+                    </div>
+                    <div class="TxtCard">
+                        Wipeout
+                    </div>
+                    <div class="TxtColor">
+                        <div class="TxtCard">
+                            Kom mee naar wipeout, voor een leuke en gezellige middag
+                        </div>
+                    </div>
+                </a>
+            </div>
     </div>
 </body>
 </html>
