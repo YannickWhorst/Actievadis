074f4bb8e4f08f6b6eb5b6723df815d36e4a9473
rudemoos <m.bloemendaal210@gmail.com>
2023-10-04 15:30:44 +0200

    Add required

--- App/activiteitenInvoeg.php
+++ App/activiteitenInvoeg.php
@@ -42,52 +42,52 @@ if(isset($_POST['voeg'])) {
         <h1>Voeg activiteit toe</h1>
         <form method="post" action="#" enctype="multipart/form-data">
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="naam" name="naam" placeholder="Naam">
-                <label for="naam">Naam</label>
+                <input type="text" class="form-control" id="naam" name="naam" placeholder="Naam" required>
+                <label for="naam">Naam *</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="locatie" name="locatie" placeholder="Locatie">
-                <label for="locatie">Locatie</label>
+                <input type="text" class="form-control" id="locatie" name="locatie" placeholder="Locatie" required>
+                <label for="locatie">Locatie *</label>
             </div>
             <div class="form-check mb-3">
                 <input class="form-check-input" type="checkbox" id="eten" name="eten">
                 <label class="form-check-label" for="eten">Eten</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="number" class="form-control" id="maxDeelnemers" name="maxDeelnemers" placeholder="1">
-                <label for="maxDeelnemers">Maximale deelnemers</label>
+                <input type="number" class="form-control" id="maxDeelnemers" name="maxDeelnemers" placeholder="1" required>
+                <label for="maxDeelnemers">Maximale deelnemers *</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="number" class="form-control" id="minDeelnemers" name="minDeelnemers" placeholder="1">
-                <label for="minDeelnemers">Minimale deelnemers</label>
+                <input type="number" class="form-control" id="minDeelnemers" name="minDeelnemers" placeholder="1" required>
+                <label for="minDeelnemers">Minimale deelnemers *</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="kosten" name="kosten" placeholder="Kosten">
-                <label for="kosten">Kosten</label>
+                <input type="text" class="form-control" id="kosten" name="kosten" placeholder="Kosten" required>
+                <label for="kosten">Kosten *</label>
             </div>
             <div class="form-floating mb-3">
                 <input type="text" class="form-control" id="benodigdheden" name="benodigdheden" placeholder="Benodigdheden">
                 <label for="benodigdheden">Benodigdheden</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="omschrijving" name="omschrijving" placeholder="Omschrijving">
-                <label for="omschrijving">Omschrijving</label>
+                <input type="text" class="form-control" id="omschrijving" name="omschrijving" placeholder="Omschrijving" required>
+                <label for="omschrijving">Omschrijving *</label>
             </div>
             <div class="form-floating mb-3">
-                <input type="date" class="form-control" id="activiteit_datum" name="activiteit_datum" placeholder="datum">
-                <label for="activiteit_datum">activiteit datum</label>
+                <input type="date" class="form-control" id="activiteit_datum" name="activiteit_datum" placeholder="datum" required>
+                <label for="activiteit_datum">activiteit datum *</label>
             </div>
             <div class="form-floating mb-3"> 
-                <input type="time" class="form-control" id="activiteit_begin_tijd" name="activiteit_begin_tijd" placeholder="activiteit beginTijd"> 
-                <label for="activiteit_begin_tijd">startTijd</label>
+                <input type="time" class="form-control" id="activiteit_begin_tijd" name="activiteit_begin_tijd" placeholder="activiteit beginTijd" required> 
+                <label for="activiteit_begin_tijd">startTijd *</label>
             </div>
             <div class="form-floating mb-3"> 
-                <input type="time" class="form-control" id="activiteit_eindtijd" name="activiteit_eindtijd" placeholder="eindTijd"> 
-                <label for="activiteit_eindtijd">eindTijd</label>
+                <input type="time" class="form-control" id="activiteit_eindtijd" name="activiteit_eindtijd" placeholder="eindTijd" required> 
+                <label for="activiteit_eindtijd">eindTijd *</label>
             </div>
             <div class="mb-3">
-                <label for="afbeelding" class="form-label"><b>Afbeelding</b></label>
-                <input class="form-control" type="file" id="afbeelding" name="afbeelding">
+                <label for="afbeelding" class="form-label"><b>Afbeelding *</b></label>
+                <input class="form-control" type="file" id="afbeelding" name="afbeelding" required >
             </div>
             <input type="Submit" class="btn btn-primary mt-1 mb-3" name="voeg" value="Voeg toe">
         </form>
